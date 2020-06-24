c615d76472b0dc644d5408601220310a1638dc21
Francis Cho <fhcho1@yahoo.com>
2020-06-23 21:47:05 -0700

    Add about weather info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,10 @@ const forecast = (latitude,longitude, callback) => {
             callback('Unable to find location',undefined)
         } else {
             callback(undefined, 
-                body.current.weather_descriptions[0]+ ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.'
+                body.current.weather_descriptions[0]+ ' It is currently ' + body.current.temperature 
+                + ' degrees out. It feels like '+body.current.feelslike+' degrees out.  There is a ' + body.current.precip + '% chance of rain.  Humidity is '
+                + body.current.humidity + ' degrees.'
+
             )
         }
     })
